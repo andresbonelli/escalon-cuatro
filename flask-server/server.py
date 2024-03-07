da88b9323ed4909b21cf587f693d84f59a7555aa
donpipon <donpipon@hotmail.com>
2024-03-07 20:09:01 -0300

    replaced sockt.io with fetch

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,32 +1,20 @@
-from flask import Flask#, render_template, jsonify
-from flask_socketio import SocketIO, emit
+from flask import Flask, request, jsonify
 from flask_cors import CORS
-import eventlet
 from game import Game
 
-
 app = Flask(__name__)
-#app.config['SECRET_KEY'] = 'your_secret_key' 
-socketio = SocketIO(app, async_mode='eventlet', cors_allowed_origins="*")
 CORS(app)
 
 game = Game()
 
-"""@app.route('/')
-def index():
-    return render_template('index.html')"""
-
-@socketio.on('connect')
-def handle_connect():
-    print('Client connected')
-
-@socketio.on('get_word')
-def start_game():
+@app.route('/get_word')
+def get_word():
     data = game.run()
     print(data)
-    emit('word_data', {'data': data})
-    
+    return jsonify(data)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    app.run(debug=True)
+    
+    
     
